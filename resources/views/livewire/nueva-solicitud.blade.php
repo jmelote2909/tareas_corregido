af377067744fbca43af144d3aea318aa0b97038d
jmelote2909 <jmelote2909@iesmarquesdecomares.org>
2026-05-18 11:47:01 +0200

    c

--- resources/views/livewire/nueva-solicitud.blade.php
+++ resources/views/livewire/nueva-solicitud.blade.php
@@ -152,7 +152,7 @@
                                         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><polyline points="17 8 12 3 7 8"/><line x1="12" x2="12" y1="3" y2="15"/></svg>
                                         Subir Audio
                                     </div>
-                                    <input type="file" wire:model="audio" class="hidden" accept="audio/*, .mp3, .wav, .m4a, .aac, .ogg, .wma">
+                                    <input type="file" wire:model="audio" class="hidden" accept=".mp3,.wav,.m4a,.aac,.ogg,.wma,.amr,.flac,.opus,.caf,.weba,.webm">
                                 </label>
 
                                 {{-- Document File --}}
